67449f6d97455650e0d67e70b2a49e8a1c212f30
Axel <axelvalenzuela7@gmail.com>
2018-09-12 22:42:49 -0500

    Segundo cambio 12/09/2018 Problema con los try chach y validacion de 0 y 1

--- app/src/main/java/com/example/axelyray/tramas/MainActivity.java
+++ app/src/main/java/com/example/axelyray/tramas/MainActivity.java
@@ -23,56 +23,72 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     public void BinCRC(){
         EditText IngCRC=(EditText)findViewById(R.id.IngCRC);
-        String DatCRC=IngCRC.getText().toString();
-        //opbinario
-        int dec=Integer.parseInt(DatCRC,2);
-        String val=Integer.toHexString(dec).toUpperCase();
-        System.out.println("HEXADECIMAL11: "+val);
+        if(IngCRC.getText().toString().length()==16) {
+            char [] arrCRC=IngCRC.getText().toString().toCharArray();
+                String DatCRC = IngCRC.getText().toString();
+                //opbinario
+                int dec = Integer.parseInt(DatCRC, 2);
+                String val = Integer.toHexString(dec).toUpperCase();
+                System.out.println("HEXADECIMAL12: " + val);
+                System.out.println("Funciona");
+        }
+        else{
+            IngCRC.setText("0000000000000000");
+        }
     }
 
     public void BinETX(){
         EditText IngETX=(EditText)findViewById(R.id.IngETX);
-        String DatETX=IngETX.getText().toString();
-        //opBinario
-        int dec=Integer.parseInt(DatETX,2);
-        String val=Integer.toHexString(dec).toUpperCase();
-        System.out.println("HEXADECIMAL11: "+val);
+        if(IngETX.getText().toString().length()==8) {
+                String DatETX = IngETX.getText().toString();
+                //opBinario
+                int dec = Integer.parseInt(DatETX, 2);
+                String val = Integer.toHexString(dec).toUpperCase();
+                System.out.println("HEXADECIMAL11: " + val);
+
+        }else{
+            IngETX.setText("00000000");
+        }
     }
 
-    public void BinCabecera(){
+    public String BinCabecera(){
         EditText IngCabecera=(EditText)findViewById(R.id.IngCabecera);
         String DatCabecera=IngCabecera.getText().toString();
         //opBinario
         int dec=Integer.parseInt(DatCabecera,2);
         String val=Integer.toHexString(dec).toUpperCase();
         System.out.println("HEXADECIMAL11: "+val);
+        return val;
     }
 
-    public void BinSYN(){
+    public String BinSYN(){
         EditText IngSYN=(EditText)findViewById(R.id.SYN);
         String DatSYN=IngSYN.getText().toString();
         //opBinario
         int dec=Integer.parseInt(DatSYN,2);
         String val=Integer.toHexString(dec).toUpperCase();
         System.out.println("HEXADECIMAL11: "+val);
+        return val;
     }
 
-    public void BinSOH(){
+    public String BinSOH(){
         EditText IngSOH=(EditText)findViewById(R.id.IngSOH);
         String DatSOH=IngSOH.getText().toString();
         //opBinario
         int dec=Integer.parseInt(DatSOH,2);
         String val=Integer.toHexString(dec).toUpperCase();
         System.out.println("HEXADECIMAL11: "+val);
+        return val;
     }
 
-    public void BinSTX(){
+    public String BinSTX(){
         EditText IngSTX=(EditText)findViewById(R.id.IngSTX);
         String DatSTX=IngSTX.getText().toString();
         //opBinario
         int dec=Integer.parseInt(DatSTX,2);
         String val=Integer.toHexString(dec).toUpperCase();
         System.out.println("HEXADECIMAL11: "+val);
+        return  val;
     }
 
     @Override
@@ -82,42 +98,47 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         String Arrtotal[];
 
         EditText MostValor= (EditText)findViewById(R.id.MostValor);
+        EditText MostValor2 =(EditText)findViewById(R.id.MostValor2);
         EditText IngValor = (EditText)findViewById(R.id.IngValor);
 
+        if(IngValor.getText().toString().isEmpty()){
+            IngValor.setText("Ingrese Frase");
+        }else {
+            BinCRC();
+            BinETX();
+            String RCabecera=BinCabecera();
+            String RBinSYN =BinSYN();
+            String RSOH =BinSOH();
+            String RSTX =BinSTX();
 
-        BinCRC();
-        BinETX();
-        BinCabecera();
-        BinSYN();
-        BinSOH();
-        BinSTX();
+            String cade = IngValor.getText().toString();
+            int tam = cade.length();
+            a = new int[tam];
+            Arrtotal = new String[tam];
+            for (int x = 0; x < cade.length(); x++) {
+                int tamanio = IngValor.length();
+                System.out.println(cade.charAt(x) + " = " + cade.codePointAt(x));
+                int asc = cade.codePointAt(x);
+                a[x] = asc;
+                //ascci(asc,tamanio);
+            }
 
-        String cade = IngValor.getText().toString();
-        int tam=cade.length();
-        a=new int[tam];
-        Arrtotal=new String[tam];
-        for (int x=0;x<cade.length();x++) {
-            int tamanio=IngValor.length();
-            System.out.println(cade.charAt(x) + " = " + cade.codePointAt(x));
-            int asc = cade.codePointAt(x);
-            a[x]=asc;
-            //ascci(asc,tamanio);
+            for (int i = 0; i < tam; i++) {
+                int b = a[i];
+                String hexa = Integer.toHexString(b).toUpperCase();
+                Arrtotal[i] = hexa;
+            }
+
+            for (int i = 0; i < tam; i++) {
+                System.out.print(Arrtotal[i]);
+
+            }
+            String ga = String.valueOf(Arrtotal);
+            System.out.println("valor :" + ga);
+            MostValor.setText("Valor de STX: "+RSTX+"\n\t"+"Valor de SOH: "+RSOH+"\n");
+            MostValor2.setText("Valor de SYN: "+RBinSYN+"\n"+"Valor de Cabecera: "+RCabecera);
+            cade = null;
+
         }
-
-        for(int i=0;i<tam;i++){
-            int b=a[i];
-            String hexa=Integer.toHexString(b).toUpperCase();
-            Arrtotal[i]=hexa;
-        }
-
-        for (int i=0;i<tam;i++){
-            System.out.print(Arrtotal[i]);
-
-        }
-        String ga= String.valueOf(Arrtotal).toString();
-        System.out.println("valor :"+ga);
-        MostValor.setText(ga);
-        cade=null;
-
     }
 }
